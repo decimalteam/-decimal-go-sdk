241addc4c7711cc13509ac73385365c91892ed2b
igrshalygin <igrshalygin@gmail.com>
2021-07-14 12:37:04 +0700

    add missing nft querying

---
+++ api/nft.go
@@ -0,0 +1,50 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type NFT struct {
+	ID          int         `json:"id"`
+	Slug        string      `json:"slug"`
+	Headline    string      `json:"headline"`
+	Description string      `json:"description"`
+	Misc        interface{} `json:"misc"`
+	Cover       string      `json:"cover"`
+	Asset       string      `json:"asset"`
+	Status      string      `json:"status"`
+	CreatedAt   string      `json:"createdAt"`
+	UpdatedAt   string      `json:"updatedAt"`
+}
+
+type NFTResponse struct {
+	OK     bool `json:"ok"`
+	Result struct {
+		Token *NFT
+	}
+}
+
+func (api *API) NFT(id string) (*NFT, error) {
+	url := fmt.Sprintf("/nfts/%s", id)
+	res, err := api.client.R().Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if res.IsError() {
+		return nil, NewResponseError(res)
+	}
+
+	response := NFTResponse{}
+	err = json.Unmarshal(res.Body(), &response)
+	if err != nil || !response.OK {
+		responseError := Error{}
+		err = json.Unmarshal(res.Body(), &responseError)
+		if err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("received response containing error: %s", responseError.Error())
+	}
+
+	return response.Result.Token, nil
+}
